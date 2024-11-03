3b75f3a40293cdb8eb12b5442a8b283de79df4d3
BOGA\bogas <bogacnutctk@gmail.com>
2024-11-03 23:05:46 +0530

    remove unwanted imports from controller

--- src/main/java/controller/ExampleController.java
+++ src/main/java/controller/ExampleController.java
@@ -1,6 +1,5 @@
 package controller;
 
-import exception.CustomException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
