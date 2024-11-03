7c45eba22f6649058111c89630ecde603439a976
BOGA\bogas <bogacnutctk@gmail.com>
2024-11-03 23:06:21 +0530

    add EqualsAndHashCode for smart use

--- src/main/java/exception/CustomException.java
+++ src/main/java/exception/CustomException.java
@@ -1,5 +1,23 @@
 package exception;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+/*
+Why Is This Important?
+Equality: If you have a class that extends another class (like Child extends Parent),
+you want to ensure that when you compare two Child objects, the comparison also
+considers the properties of Parent. If you don’t do this, two Child objects could be
+considered equal even if their parent properties are different.
+
+Hash Code: The hashCode() method is important for storing objects in collections like
+HashMap. If two objects are considered equal (via equals()), they must return the same
+hash code. This helps with efficient data retrieval.
+*/
+
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class CustomException extends RuntimeException {
 	private String message;
 	private int statusCode;
@@ -10,11 +28,4 @@ public class CustomException extends RuntimeException {
 		this.statusCode = statusCode;
 	}
 	
-	public String getMessage() {
-		return message;
-	}
-	
-	public int getStatusCode() {
-		return statusCode;
-	}
 }
